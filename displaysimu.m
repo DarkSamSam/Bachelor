d0bc50461aabcf8c9f9902fa306363074e437ab5
DarkSamSam <samsekarski@bluewin.ch>
2017-06-14 13:16:25 +0200

    minor improvements
    comments

--- displaysimu.m
+++ displaysimu.m
@@ -3,9 +3,11 @@ function displaysimu(t,y,u,filename)   %plots the simulation in graphs
 %   t: vector of sampling times
 %   y: corresponding state values of a system at sampling times of t
 %   u: input at sampling times of t
+%   filename: string containing the filename where to save the graph, '' will not save anything
 %OUTPUT:
 %   none as of yet
 
+%states vs time plot
 subplot(2,1,1);
 plot(t,y(:,1),'b',t,y(:,2),'r',t,y(:,3),'g',t,y(:,4),'k');
 title('State variables variation');
@@ -13,6 +15,7 @@ legend('x [m]','dx [m/s]','theta [rad]','dtheta [rad/s]','input [N]');
 xlabel('time[s]');
 ylabel('value');
 
+%Input vs time plot
 subplot(2,1,2);
 plot(t,u(:),'b');
 title('Input');
@@ -21,7 +24,7 @@ xlabel('time[s]');
 ylabel('value');
 
 if ~isempty(filename)
-    print(filename,'-dpng');    %save graph to file
+    print(filename,'-dpng');    %save graph to file as filename.png
 end
 
 end
