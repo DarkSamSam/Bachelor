48a448675568924e833337ecc18c68b197dbcbe1
DarkSamSam <samsekarski@bluewin.ch>
2017-06-14 13:19:02 +0200

    minor fixes
    comments

--- open_control.m
+++ open_control.m
@@ -2,14 +2,13 @@ function [t,y,output]=open_control(eqs, x, tspan, y0, h)
 %INPUT:
 %   eqs: discrete system equations, either linear or non linear
 %   x: state variables
-%   tspan: length of time so simulate [s]
-%   y0: initial sates
+%   tspan: length of time to simulate [s]
+%   y0: initial states
 %   h: time step
 %OUTPUTS:
 %   t: vector of sampling times
 %   y: vector of solutions the state equation at sampling times of t
-%   output: vector of controller inputs at sampling time t, which here is a
-%   null vecotor, since there is no controller
+%   output: vector of controller inputs at sampling time t, which here is a null vecotor, since there is no controller
 
 syms u
 eqs = subs(eqs);
@@ -18,12 +17,15 @@ f = matlabFunction(eqs, 'Vars', {u,[x(1);x(2);x(3);x(4)]}); %transforms symbolic
 n = floor(tspan/h);
 y = zeros(n,4);
 t = zeros(1,n);
+
 %intital setup
 y(1,1) = y0(1);
 y(1,2) = y0(2);
 y(1,3) = y0(3);
 y(1,4) = y0(4);
 output = zeros(1,n);
+
+%time simulation
 for i=2:n
     xx = f(output(i), y(i-1,:)');
     y(i,1) = xx(1);
