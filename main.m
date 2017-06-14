cd321649aa6ed6696151ad8a81d70fbb8cff0c37
DarkSamSam <samsekarski@bluewin.ch>
2017-06-14 13:10:05 +0200

    better
    minor fixes and improvements

--- main.m
+++ main.m
@@ -22,8 +22,11 @@ paraMot.Km = 0.01; % motor torque constant [Nm/A] or [V/(rad s)]
 paraMot.R = 1; % internal resistance [Ohm]
 paraMot.r = 0.025; % radius of the wheels [m]
 
+%simulation parameters:
+paraSimu.tspan = 10; %simulation interval in seconds
+paraSimu.y0 = [0 0 0.5 0];   %initial conditions on states, here the robot start with a tilt of 0.5rad
+
 %% Forces and levers
-%note to self: maybe put all this in external script/function ?
 syms N P positive %internal cohesion forces
 Mg = [0;para.M*para.g;0]; %gravific force
 T = [N;P;0]; %internal force
@@ -33,60 +36,63 @@ F_motor = motor_force(u,state,paraMot);
 mg = [0;para.m*para.g;0]; %gravific force
 Fin = [-para.m*f2xu;0;0]; %inertia "force" on pendulum from cart acceleration 
 
-l1 = cross(-[sin(state(3));cos(state(3));0]*para.L,-T); %lever exerted by cart on pendulum
+l1 = cross(-[sin(state(3));cos(state(3));0]*para.L,-T); %torq exerted by cart on pendulum
 
 forces = cat(3,[Mg,T,F_motor],[-T,mg,Fin]); %all forces of system on 1 line, every system is new line
-levers = cat(3,zeros(3,1),l1); %all levers of system on 1 line, every system is new line
+levers = cat(3,zeros(3,1),l1); %all torqs of system on 1 line, every system is new line
 
 %% Non linear equations
 fxu = dyn(state,fxu,forces,levers,para); %non linear state equations
 
 %% Discretization
-disc_eqs = discri(fxu,state,'RK');
+disc_eqs = discri(fxu,state,'RK'); %'RK' uses Kunge-Kutta 4, 'FE' uses forward Euler method
 syms h
 h = para.h;
 disc_eqs = subs(disc_eqs);
 
 %% Linearization
-% [A,B,C,D] = lini(fxu,state,u);   %returns linear matrices, usung first order Taylor approximation
-% fxu_l = A*state + B*u; %linear state equations
-[phi,gamma,C,D] = lini(disc_eqs,state,u);
-disc_eqs_l = phi*state + gamma*u;
+% [A,B,C,D] = lini(fxu,state,u);   %returns linear matrices, usung first order Taylor approximation (deprecated but still works)
+% fxu_l = A*state + B*u; %linear state equations (deprecated, but still works)
+[phi,gamma,C,D] = lini(disc_eqs,state,u); %returns discrete linear matrices, using 1st order Taylor approx
+disc_eqs_l = phi*state + gamma*u;   %discrete linear state equations
 
-%% Stablility analysis
+%% Open Loop simulation
+  [open_times,open_states,open_feedback] = open_control(disc_eqs,state, paraSimu.tspan, paraSimu.y0, para.h); 
 
-%% Open Loop
-   tspan = [0 20]; %simulation interval, this will need to change a little, after discretization is implemented
-  y0 = [0 0 0.5 0];   %initial conditions on states
-  [open_times,open_states,open_feedback] = open_control(disc_eqs,state, 10, y0, para.h); %last input should be filename if graph is to be saved
+%% PID closed loop simulation
+gainsPID.p = 60;
+gainsPID.i = 1;
+gainsPID.d = 20;
+  [pid_times,pid_states,pid_feedback] = pid_control(disc_eqs,state,0,paraSimu.y0,gainsPID.p,gainsPID.i,gainsPID.d,paraSimu.tspan,para.h);
+% [pid_l_times,pid_l_states,pid_l_feedback] = pid_control(disc_eqs_l,state,0,paraSimu.y0,gainsPID.p,gainsPID.i,gainsPID.d,paraSimu.tspan,para.h);
 
 
-%% PID
-% [pid_times,pid_states,pid_feedback] = pid_control(disc_eqs,state,0,y0,60,1,20,10,para.h);
-
-
-%% LQR
+%% LQR closed loop simulation
  Q = C'*C;
  Q(1,1) = 1; % x position weight in LQR
  Q(3,3) = 500;   % theta angle weight in LQR
  R = 1;  %weight of input in LQR
-[lqr_times,lqr_states,lqr_feedback] = lqr_control(phi,gamma,Q,R,y0,10,para.h);
+[lqr_times,lqr_states,lqr_feedback] = lqr_control(phi,gamma,Q,R,paraSimu.y0,paraSimu.tspan,para.h);
 
 %% Graphs
 %note: figure ids 40-49 are reserved for open loop figures, 50-59 for pid
 %contolled figures, and 60-69 for lqr controlled figures
-% figure(40);
-% displaysimu(open_times,open_states,open_feedback,''); %last input should be a filename if a gif is wanted
-% figure(50);
-% displaysimu(pid_times,pid_states,pid_feedback,'');
+%last input should be a filename if a PNG is wanted
+ figure(40);
+ displaysimu(open_times,open_states,open_feedback,'');
+ figure(50);
+ displaysimu(pid_times,pid_states,pid_feedback,'');
 % figure(51);
 % displaysimu(pid_l_times,pid_l_states,pid_l_feedback,'');
-% figure(60);
-% displaysimu(lqr_times,lqr_states,lqr_feedback,'');
+ figure(60);
+ displaysimu(lqr_times,lqr_states,lqr_feedback,'');
 
 %% Animation
+%Warning, the animations are not real time. They are much slower, for some reason I cannot put a pause time
+%smaller than 0.01 seconds, therefore a 10 second simulation will take 10*1200*0.01 = 2 minutes.
+%last input should be a filename if a gif is wanted
 % figure(41);
-% anim(open_times,open_states,para,'');   %last input should be a filename if a gif is wanted
+% anim(open_times,open_states,para,'');
 % figure(52);
 % anim(pid_times,pid_states,para,'');
 % figure(53);
